0402c536c6f53967bcadadb6329a21ee356f314a
Renê Schneider <renews@gmail.com>
2014-12-16 17:28:58 -0200

    debug

--- lib/delayed_paperclip/jobs/delayed_job.rb
+++ lib/delayed_paperclip/jobs/delayed_job.rb
@@ -3,15 +3,15 @@ require 'delayed_job'
 module DelayedPaperclip
   module Jobs
     class DelayedJob < Struct.new(:instance_klass, :instance_id, :attachment_name)
-      RAILS.logger.debug '=[=========================================================='
-      RAILS.logger.debug 'delayed_job.rb'
+      Logger.new(STDOUT).debug '=[=========================================================='
+      Logger.new(STDOUT).debug 'delayed_job.rb'
     # extend DelayedPaperclip::Jobs::HerokuJob
       def initialize
         if Rails.env.production?
           @heroku ||= HerokuManager.new
-          RAILS.logger.debug('initializer')
+          Logger.new(STDOUT).debug('initializer')
           @heroku.set_workers(1) if (@heroku.get_workers == 0)
-          RAILS.logger.debug('workers'<<@heroku.get_workers.to_s)
+          Logger.new(STDOUT).debug('workers'<<@heroku.get_workers.to_s)
           sleep 10
         end
       end
@@ -19,23 +19,23 @@ module DelayedPaperclip
       def after(job)
         if Rails.env.production?
           @heroku ||= HerokuManager.new
-          RAILS.logger.debug 'after'
+          Logger.new(STDOUT).debug 'after'
           @heroku.set_workers(0) if (job_count == 1)
-          RAILS.logger.debug 'workers'<<@heroku.get_workers.to_s
+          Logger.new(STDOUT).debug 'workers'<<@heroku.get_workers.to_s
         end
       end
 
       def job_count
-        RAILS.logger.debug 'job count'
-        RAILS.logger.debug(Delayed::Job.where('failed_at IS NULL').length)
+        Logger.new(STDOUT).debug 'job count'
+        Logger.new(STDOUT).debug(Delayed::Job.where('failed_at IS NULL').length)
         Delayed::Job.where('failed_at IS NULL').length
       end
 
       if Gem.loaded_specs['delayed_job'].version >= Gem::Version.new("2.1.0") # this is available in newer versions of DelayedJob. Using the newee Job api thus.
-        RAILS.logger.debug 'gem  newer'
+        Logger.new(STDOUT).debug 'gem  newer'
 
         def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-          RAILS.logger.debug('='<<instance_klass.to_s<<instance_id.to_s<<attachment_name.to_s)
+          Logger.new(STDOUT).debug('='<<instance_klass.to_s<<instance_id.to_s<<attachment_name.to_s)
           ::Delayed::Job.enqueue(
             :payload_object => new(instance_klass, instance_id, attachment_name),
             :priority => instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:priority].to_i,
@@ -44,9 +44,9 @@ module DelayedPaperclip
         end
 
       else
-        RAILS.logger.debug 'gem old'
+        Logger.new(STDOUT).debug 'gem old'
         def self.enqueue_delayed_paperclip(instance_klass, instance_id, attachment_name)
-          RAILS.logger.debug('='<<instance_klass.to_s<<instance_id.to_s<<attachment_name.to_s)
+          Logger.new(STDOUT).debug('='<<instance_klass.to_s<<instance_id.to_s<<attachment_name.to_s)
           ::Delayed::Job.enqueue(
             new(instance_klass, instance_id, attachment_name),
             instance_klass.constantize.paperclip_definitions[attachment_name][:delayed][:priority].to_i,
@@ -57,11 +57,11 @@ module DelayedPaperclip
       end
 
       def perform
-        RAILS.logger.debug 'perform'
-        RAILS.logger.debug('perform='<<instance_klass.to_s<<instance_id.to_s<<attachment_name.to_s)
+        Logger.new(STDOUT).debug 'perform'
+        Logger.new(STDOUT).debug('perform='<<instance_klass.to_s<<instance_id.to_s<<attachment_name.to_s)
         DelayedPaperclip.process_job(instance_klass, instance_id, attachment_name)
       end
-      @RAILS.logger.debug '=[=========================================================='
+      @Logger.new(STDOUT).debug '=[=========================================================='
     end
   end
 end
